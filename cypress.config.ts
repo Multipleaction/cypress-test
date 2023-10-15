1db0eb57b54c0439c56c34c4ddb8fac0c00760c1
Dmytro2105 <dmytro@triptomatic.com>
2023-10-15 15:35:11 +0300

    First project headless run with reports

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,19 @@
-import { defineConfig } from "cypress";
+import {defineConfig} from 'cypress';
 
 export default defineConfig({
 	viewportWidth: 1920, // Set the desired viewport width
 	viewportHeight: 1080, // Set the desired viewport height
-
+	reporter: 'mochawesome',
+	reporterOptions: {
+		reportDir: 'cypress/results',
+		overwrite: false,
+		html: false,
+		json: true,
+	},
 	e2e: {
 		setupNodeEvents(on, config) {
 			// Implement node event listeners here
 		},
-		baseUrl: "http://localhost:4200/",
+		baseUrl: 'http://localhost:4200/',
 	},
 });
