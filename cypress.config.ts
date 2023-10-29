5e8f7c004ffba0b5134b1db20a44e71e4e824abe
Dmytro2105 <dmytro@triptomatic.com>
2023-10-29 13:49:24 +0200

    added workwlow

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import {defineConfig} from 'cypress';
 
 export default defineConfig({
+	projectId: 'qhtyz1',
 	viewportWidth: 1920, // Set the desired viewport width
 	viewportHeight: 1080, // Set the desired viewport height
 	reporter: 'mochawesome',
